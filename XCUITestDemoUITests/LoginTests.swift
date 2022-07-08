8964876006aaeefbc0107dd22c98747e95629942
Christian Vasquez <christianvasquezmelo@gmail.com>
2022-07-07 22:26:48 -0400

    Refactor code

--- XCUITestDemoUITests/LoginTests.swift
+++ XCUITestDemoUITests/LoginTests.swift
@@ -1,16 +1,15 @@
 import XCTest
 
 class LoginTests: BaseTest {
+    lazy var login = LoginScreen(app: app)
     
     func testLogin_InvalidCredentials_ShowsAlertDialog() {
-        let login = LoginScreen(app: app)
         login.signIn(username: "wrongUsername", password: "wrongPassword")
         
         XCTAssert(login.hasInvalidCredentials())
     }
     
     func testLogin_ValidCredentials_RedirectToNextScreen() {
-        let login = LoginScreen(app: app)
         login.signIn(username: "Chris", password: "pass123")
         let home = HomeScreen(app: app)
         
