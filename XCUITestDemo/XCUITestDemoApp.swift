3ab8d019b6904dc6c88877da47a948a83c7c1862
Christian Vasquez <christianvasquezmelo@gmail.com>
2022-07-07 20:31:44 -0400

    Initial Commit

---
+++ XCUITestDemo/XCUITestDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCUITestDemoApp.swift
+//  XCUITestDemo
+//
+//  Created by Christian Vasquez on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct XCUITestDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
